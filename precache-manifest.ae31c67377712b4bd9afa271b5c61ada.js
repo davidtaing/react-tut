31ccf98651b4ae792f1880c64f3d214e352a575d
David Taing <adavidtaing@gmail.com>
2020-02-01 17:49:24 +1100

    Updates

---
+++ precache-manifest.ae31c67377712b4bd9afa271b5c61ada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b1e82b3d20a915e0f754f24f2220a8",
+    "url": "/davidtaing/react-tut/index.html"
+  },
+  {
+    "revision": "2297b6abf7745115ba22",
+    "url": "/davidtaing/react-tut/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "484d7e1a873b04782353",
+    "url": "/davidtaing/react-tut/static/js/2.64aa8813.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/davidtaing/react-tut/static/js/2.64aa8813.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2297b6abf7745115ba22",
+    "url": "/davidtaing/react-tut/static/js/main.2ab715a1.chunk.js"
+  },
+  {
+    "revision": "b22cfe2247d1653c8635",
+    "url": "/davidtaing/react-tut/static/js/runtime-main.10a1d500.js"
+  }
+]);
